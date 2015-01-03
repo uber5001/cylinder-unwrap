185296a601ef7d47a4a7db6e73afff615abfe0c1
Scott Little <uber5001@gmail.com>
2015-01-03 04:39:19 -0500

    add somewhat working texture

--- index.js
+++ index.js
@@ -7,8 +7,10 @@ gl.viewportHeight = canvas.height;
 var fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
 gl.shaderSource(fragmentShader,
 	"precision mediump float;"
+	+ "varying highp vec2 vTextureCoord;"
+	+ "uniform sampler2D uSampler;"
 	+ "void main(void) {"
-		+ "gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);"
+		+ "gl_FragColor = texture2D(uSampler, vec2(vTextureCoord.s, vTextureCoord.t));"
 	+ "}"
 );
 gl.compileShader(fragmentShader);
@@ -17,24 +19,16 @@ gl.compileShader(fragmentShader);
 var vertexShader = gl.createShader(gl.VERTEX_SHADER);
 gl.shaderSource(vertexShader, 
 	"attribute vec3 aVertexPosition;"
+	+ "attribute vec2 aTextureCoord;"
     + "uniform mat4 uMVMatrix;"
 	+ "uniform mat4 uPMatrix;"
+	+ "varying highp vec2 vTextureCoord;"
 	+ "void main(void) {"
 		+ "gl_Position = uPMatrix * uMVMatrix * vec4(aVertexPosition, 1.0);"
+		+ "vTextureCoord = aTextureCoord;"
 	+ "}"
 );
 gl.compileShader(vertexShader);
-
-//triangle
-var triangle = gl.createBuffer();
-gl.bindBuffer(gl.ARRAY_BUFFER, triangle);
-gl.bufferData(gl.ARRAY_BUFFER, new Float32Array([
-	0, 1, 0,
-	-1,-1,0,
-	1, -1,0
-]), gl.STATIC_DRAW);
-triangle.itemSize = 3;
-triangle.numItems = 3;
 
 //shader program?
 var shaderProgram = gl.createProgram();
@@ -58,13 +52,44 @@ var mvMatrixUniform = gl.getUniformLocation(shaderProgram, "uMVMatrix");
 var square = gl.createBuffer();
 gl.bindBuffer(gl.ARRAY_BUFFER, square);
 gl.bufferData(gl.ARRAY_BUFFER, new Float32Array([
-	1, 1, 0,
-	-1,1, 0,
-	1, -1,0,
-	-1,-1,0
+	-1,-1, 0,
+	 1,-1, 0,
+	 1, 1, 0,
+	-1, 1, 0
 ]), gl.STATIC_DRAW);
-square.itemSize = 3;
-square.numItems = 4;
+
+//textures
+var texture = gl.createTexture();
+var image = new Image();
+image.onload = resume;
+image.src = "crate.jpg";
+							function resume() {
+gl.bindTexture(gl.TEXTURE_2D, texture);
+gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, image);
+gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
+gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR_MIPMAP_NEAREST);
+gl.generateMipmap(gl.TEXTURE_2D);
+gl.bindTexture(gl.TEXTURE_2D, null);
+
+//texture mapping
+var squareTexture = gl.createBuffer();
+gl.bindBuffer(gl.ARRAY_BUFFER, squareTexture);
+gl.bufferData(gl.ARRAY_BUFFER, new Float32Array([
+	0,0,
+	1,0,
+	1,1,
+	0,1
+]), gl.STATIC_DRAW);
+
+var textureCoordAttribute = gl.getAttribLocation(shaderProgram, "aTextureCoord");
+gl.enableVertexAttribArray(textureCoordAttribute);
+
+gl.bindBuffer(gl.ARRAY_BUFFER, squareTexture);
+gl.vertexAttribPointer(textureCoordAttribute, 2, gl.FLOAT, false, 0, 0);
+
+gl.activeTexture(gl.TEXTURE0);
+gl.bindTexture(gl.TEXTURE_2D, texture);
+gl.uniform1i(gl.getUniformLocation(shaderProgram, "uSampler"), 0);
 
 //clear scene before draw?
 gl.clearColor(0,0,0,1);
@@ -89,31 +114,13 @@ mat4.perspective(
 //reset the draw location to the center of the screen
 mat4.identity(mvMatrix);
 
-//draw the triangle 1.5 to the left
-mat4.translate(mvMatrix, mvMatrix, [-1.5, 0, -7]);
-gl.bindBuffer(gl.ARRAY_BUFFER, triangle);
-//set the verticies to be an array?
-gl.vertexAttribPointer(
-	vertexPositionAttribute,
-	triangle.itemSize,
-	gl.FLOAT,
-	false,
-	0,
-	0
-);
-//set the uniforms (position)
-gl.uniformMatrix4fv(pMatrixUniform, false, pMatrix);
-gl.uniformMatrix4fv(mvMatrixUniform, false, mvMatrix);
-//and draw!
-gl.drawArrays(gl.TRIANGLES, 0, triangle.numItems);
-
-//draw the square 1.5 to the right
-mat4.translate(mvMatrix, mvMatrix, [3,0,0]);
+//draw the square
+mat4.translate(mvMatrix, mvMatrix, [0,0,-2]);
 gl.bindBuffer(gl.ARRAY_BUFFER, square);
 //set the vertices to be an array?
 gl.vertexAttribPointer(
 	vertexPositionAttribute,
-	square.itemSize,
+	3, //the array is of vec3s (all concatenated into one array)
 	gl.FLOAT,
 	false,
 	0,
@@ -123,9 +130,6 @@ gl.vertexAttribPointer(
 gl.uniformMatrix4fv(pMatrixUniform, false, pMatrix);
 gl.uniformMatrix4fv(mvMatrixUniform, false, mvMatrix);
 //and draw!
-gl.drawArrays(gl.TRIANGLE_STRIP, 0, square.numItems);
+gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4 /* vert count */);
 
-
-
-
-
+						}
